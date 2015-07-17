5e9b3c15dbea52ac1493373c6779e42e45e962b9
chaosink <475936052@qq.com>
2015-07-17 21:23:00 +0800

    matrices standardized

--- visual.cpp
+++ visual.cpp
@@ -25,6 +25,8 @@ using namespace glm;
 
 const int fps = 60;
 const int cube_height = 4;
+const int waveform_interval = 1;
+const float waveform_length = 2.0;
 
 void *play_wav_d(void *file) {
 	play_wav((char *)file);
@@ -35,9 +37,9 @@ int main(int argc, char **argv)
 {
 	if(argc != 2) exit(0);
 	// Initialise GLFW
-	if( !glfwInit() )
+	if(!glfwInit())
 	{
-		fprintf( stderr, "Failed to initialize GLFW\n" );
+		fprintf(stderr, "Failed to initialize GLFW\n");
 		return -1;
 	}
 
@@ -47,9 +49,9 @@ int main(int argc, char **argv)
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	// Open a window and create its OpenGL context
-	window = glfwCreateWindow( 1024, 768, "Tutorial 04 - Colored Cube", NULL, NULL);
-	if( window == NULL ){
-		fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
+	window = glfwCreateWindow( 1920, 1080, "Audio Visualization", glfwGetPrimaryMonitor(), NULL);
+	if(window == NULL) {
+		fprintf(stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n");
 		glfwTerminate();
 		return -1;
 	}
@@ -57,7 +59,7 @@ int main(int argc, char **argv)
 
 	// Initialize GLEW
 	glewExperimental = true; // Needed for core profile
-	if (glewInit() != GLEW_OK) {
+	if(glewInit() != GLEW_OK) {
 		fprintf(stderr, "Failed to initialize GLEW\n");
 		return -1;
 	}
@@ -102,81 +104,81 @@ int main(int argc, char **argv)
 	// Our vertices. Tree consecutive floats give a 3D vertex; Three consecutive vertices give a triangle.
 	// A cube has 6 faces with 2 triangles each, so this makes 6*2=12 triangles, and 12*3 vertices
 	static const GLfloat g_vertex_buffer_data1[] = { 
-		-1.0f-1.5f,-1.0f+1.0f,-1.0f, //left
-		-1.0f-1.5f,-1.0f+1.0f, 1.0f,
-		-1.0f-1.5f, 1.0f+1.0f, 1.0f,
-		-1.0f-1.5f,-1.0f+1.0f,-1.0f,
-		-1.0f-1.5f, 1.0f+1.0f,-1.0f,
-		-1.0f-1.5f, 1.0f+1.0f, 1.0f,
-		 1.0f-1.5f,-1.0f+1.0f,-1.0f, //right
-		 1.0f-1.5f,-1.0f+1.0f, 1.0f,
-		 1.0f-1.5f, 1.0f+1.0f, 1.0f,
-		 1.0f-1.5f,-1.0f+1.0f,-1.0f,
-		 1.0f-1.5f, 1.0f+1.0f,-1.0f,
-		 1.0f-1.5f, 1.0f+1.0f, 1.0f,
-		-1.0f-1.5f,-1.0f+1.0f, 1.0f, //front
-		 1.0f-1.5f,-1.0f+1.0f, 1.0f,
-		 1.0f-1.5f, 1.0f+1.0f, 1.0f,
-		-1.0f-1.5f,-1.0f+1.0f, 1.0f,
-		-1.0f-1.5f, 1.0f+1.0f, 1.0f,
-		 1.0f-1.5f, 1.0f+1.0f, 1.0f,
-		-1.0f-1.5f, 1.0f+1.0f,-1.0f, //top
-		 1.0f-1.5f, 1.0f+1.0f,-1.0f,
-		 1.0f-1.5f, 1.0f+1.0f, 1.0f,
-		-1.0f-1.5f, 1.0f+1.0f,-1.0f,
-		-1.0f-1.5f, 1.0f+1.0f, 1.0f,
-		 1.0f-1.5f, 1.0f+1.0f, 1.0f,
-		-1.0f-1.5f,-1.0f+1.0f,-1.0f, //back
-		 1.0f-1.5f,-1.0f+1.0f,-1.0f,
-		 1.0f-1.5f, 1.0f+1.0f,-1.0f,
-		-1.0f-1.5f,-1.0f+1.0f,-1.0f,
-		-1.0f-1.5f, 1.0f+1.0f,-1.0f,
-		 1.0f-1.5f, 1.0f+1.0f,-1.0f,
-		-1.0f-1.5f,-1.0f+1.0f,-1.0f, //bottom
-		 1.0f-1.5f,-1.0f+1.0f,-1.0f,
-		 1.0f-1.5f,-1.0f+1.0f, 1.0f,
-		-1.0f-1.5f,-1.0f+1.0f,-1.0f,
-		-1.0f-1.5f,-1.0f+1.0f, 1.0f,
-		 1.0f-1.5f,-1.0f+1.0f, 1.0f
+		-1.0f,-1.0f,-1.0f, //left
+		-1.0f,-1.0f, 1.0f,
+		-1.0f, 1.0f, 1.0f,
+		-1.0f,-1.0f,-1.0f,
+		-1.0f, 1.0f,-1.0f,
+		-1.0f, 1.0f, 1.0f,
+		 1.0f,-1.0f,-1.0f, //right
+		 1.0f,-1.0f, 1.0f,
+		 1.0f, 1.0f, 1.0f,
+		 1.0f,-1.0f,-1.0f,
+		 1.0f, 1.0f,-1.0f,
+		 1.0f, 1.0f, 1.0f,
+		-1.0f,-1.0f, 1.0f, //front
+		 1.0f,-1.0f, 1.0f,
+		 1.0f, 1.0f, 1.0f,
+		-1.0f,-1.0f, 1.0f,
+		-1.0f, 1.0f, 1.0f,
+		 1.0f, 1.0f, 1.0f,
+		-1.0f, 1.0f,-1.0f, //top
+		 1.0f, 1.0f,-1.0f,
+		 1.0f, 1.0f, 1.0f,
+		-1.0f, 1.0f,-1.0f,
+		-1.0f, 1.0f, 1.0f,
+		 1.0f, 1.0f, 1.0f,
+		-1.0f,-1.0f,-1.0f, //back
+		 1.0f,-1.0f,-1.0f,
+		 1.0f, 1.0f,-1.0f,
+		-1.0f,-1.0f,-1.0f,
+		-1.0f, 1.0f,-1.0f,
+		 1.0f, 1.0f,-1.0f,
+		-1.0f,-1.0f,-1.0f, //bottom
+		 1.0f,-1.0f,-1.0f,
+		 1.0f,-1.0f, 1.0f,
+		-1.0f,-1.0f,-1.0f,
+		-1.0f,-1.0f, 1.0f,
+		 1.0f,-1.0f, 1.0f
 	};
 
 	static const GLfloat g_vertex_buffer_data2[] = { 
-		-1.0f+1.5f,-1.0f+1.0f,-1.0f, //left
-		-1.0f+1.5f,-1.0f+1.0f, 1.0f,
-		-1.0f+1.5f, 1.0f+1.0f, 1.0f,
-		-1.0f+1.5f,-1.0f+1.0f,-1.0f,
-		-1.0f+1.5f, 1.0f+1.0f,-1.0f,
-		-1.0f+1.5f, 1.0f+1.0f, 1.0f,
-		 1.0f+1.5f,-1.0f+1.0f,-1.0f, //right
-		 1.0f+1.5f,-1.0f+1.0f, 1.0f,
-		 1.0f+1.5f, 1.0f+1.0f, 1.0f,
-		 1.0f+1.5f,-1.0f+1.0f,-1.0f,
-		 1.0f+1.5f, 1.0f+1.0f,-1.0f,
-		 1.0f+1.5f, 1.0f+1.0f, 1.0f,
-		-1.0f+1.5f,-1.0f+1.0f, 1.0f, //front
-		 1.0f+1.5f,-1.0f+1.0f, 1.0f,
-		 1.0f+1.5f, 1.0f+1.0f, 1.0f,
-		-1.0f+1.5f,-1.0f+1.0f, 1.0f,
-		-1.0f+1.5f, 1.0f+1.0f, 1.0f,
-		 1.0f+1.5f, 1.0f+1.0f, 1.0f,
-		-1.0f+1.5f, 1.0f+1.0f,-1.0f, //top
-		 1.0f+1.5f, 1.0f+1.0f,-1.0f,
-		 1.0f+1.5f, 1.0f+1.0f, 1.0f,
-		-1.0f+1.5f, 1.0f+1.0f,-1.0f,
-		-1.0f+1.5f, 1.0f+1.0f, 1.0f,
-		 1.0f+1.5f, 1.0f+1.0f, 1.0f,
-		-1.0f+1.5f,-1.0f+1.0f,-1.0f, //back
-		 1.0f+1.5f,-1.0f+1.0f,-1.0f,
-		 1.0f+1.5f, 1.0f+1.0f,-1.0f,
-		-1.0f+1.5f,-1.0f+1.0f,-1.0f,
-		-1.0f+1.5f, 1.0f+1.0f,-1.0f,
-		 1.0f+1.5f, 1.0f+1.0f,-1.0f,
-		-1.0f+1.5f,-1.0f+1.0f,-1.0f, //bottom
-		 1.0f+1.5f,-1.0f+1.0f,-1.0f,
-		 1.0f+1.5f,-1.0f+1.0f, 1.0f,
-		-1.0f+1.5f,-1.0f+1.0f,-1.0f,
-		-1.0f+1.5f,-1.0f+1.0f, 1.0f,
-		 1.0f+1.5f,-1.0f+1.0f, 1.0f
+		-1.0f,-1.0f,-1.0f, //left
+		-1.0f,-1.0f, 1.0f,
+		-1.0f, 1.0f, 1.0f,
+		-1.0f,-1.0f,-1.0f,
+		-1.0f, 1.0f,-1.0f,
+		-1.0f, 1.0f, 1.0f,
+		 1.0f,-1.0f,-1.0f, //right
+		 1.0f,-1.0f, 1.0f,
+		 1.0f, 1.0f, 1.0f,
+		 1.0f,-1.0f,-1.0f,
+		 1.0f, 1.0f,-1.0f,
+		 1.0f, 1.0f, 1.0f,
+		-1.0f,-1.0f, 1.0f, //front
+		 1.0f,-1.0f, 1.0f,
+		 1.0f, 1.0f, 1.0f,
+		-1.0f,-1.0f, 1.0f,
+		-1.0f, 1.0f, 1.0f,
+		 1.0f, 1.0f, 1.0f,
+		-1.0f, 1.0f,-1.0f, //top
+		 1.0f, 1.0f,-1.0f,
+		 1.0f, 1.0f, 1.0f,
+		-1.0f, 1.0f,-1.0f,
+		-1.0f, 1.0f, 1.0f,
+		 1.0f, 1.0f, 1.0f,
+		-1.0f,-1.0f,-1.0f, //back
+		 1.0f,-1.0f,-1.0f,
+		 1.0f, 1.0f,-1.0f,
+		-1.0f,-1.0f,-1.0f,
+		-1.0f, 1.0f,-1.0f,
+		 1.0f, 1.0f,-1.0f,
+		-1.0f,-1.0f,-1.0f, //bottom
+		 1.0f,-1.0f,-1.0f,
+		 1.0f,-1.0f, 1.0f,
+		-1.0f,-1.0f,-1.0f,
+		-1.0f,-1.0f, 1.0f,
+		 1.0f,-1.0f, 1.0f
 	};
 
 	// One color for each vertex. They were generated randomly.
@@ -307,6 +309,10 @@ int main(int argc, char **argv)
 		// Use our shader
 		glUseProgram(programID);
 
+		glEnableVertexAttribArray(0);
+		glEnableVertexAttribArray(1);
+		glEnableVertexAttribArray(2);
+		
 		computeMatricesFromInputs();
 		glm::mat4 ProjectionMatrix = getProjectionMatrix();
 		glm::mat4 ViewMatrix = getViewMatrix();
@@ -316,26 +322,22 @@ int main(int argc, char **argv)
 
 //		MVP = PV;
 		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
+		glUniform1i(objectID, 1);
 
-		float x[bpf];
-		for(int i = 0; i < bpf; i++) x[i] = 10.0 / bpf * i - 5;
-
-		glEnableVertexAttribArray(0);
-		glEnableVertexAttribArray(1);
-		glEnableVertexAttribArray(2);
-
+		float z[bpf];
+		for(int i = 0; i < bpf; i++) z[i] = waveform_length / bpf * i - waveform_length / 2;
 		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer4);
-		glBufferData(GL_ARRAY_BUFFER, bpf * 4, x, GL_STATIC_DRAW);
+		glBufferData(GL_ARRAY_BUFFER, bpf * 4, z, GL_STATIC_DRAW);
 		glVertexAttribPointer(
 			2,                  // attribute. No particular reason for 0, but must match the layout in the shader.
 			1,                  // size
 			GL_FLOAT,           // type
 			GL_FALSE,           // normalized?
-			0,                  // stride
+			waveform_interval * 4,
+								// stride
 			(void*)0            // array buffer offset
 		);
 
-		glUniform1i(objectID, 1);
 		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer3);
 		glBufferData(GL_ARRAY_BUFFER, bpf * 4, (short *)data.data + data_index, GL_DYNAMIC_DRAW);
 		glVertexAttribPointer(
@@ -343,10 +345,11 @@ int main(int argc, char **argv)
 			1,                  // size
 			GL_SHORT,           // type
 			GL_FALSE,           // normalized?
-			3,                  // stride
+			waveform_interval * 2,
+								// stride
 			(void*)0            // array buffer offset
 		);
-		glDrawArrays(GL_LINE_STRIP, 0, bpf); // 12*3 indices starting at 0 -> 12 triangles
+		glDrawArrays(GL_LINE_STRIP, 0, bpf / waveform_interval); // 12*3 indices starting at 0 -> 12 triangles
 
 		glUniform1i(objectID, 2);
 		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer3);
@@ -356,11 +359,12 @@ int main(int argc, char **argv)
 			1,                  // size
 			GL_SHORT,           // type
 			GL_FALSE,           // normalized?
-			3,                  // stride
+			waveform_interval * 2,
+								// stride
 			(void*)2            // array buffer offset
 		);
-		glDrawArrays(GL_LINE_STRIP, 0, bpf); // 12*3 indices starting at 0 -> 12 triangles
-		usleep(2000);
+		glDrawArrays(GL_LINE_STRIP, 0, bpf / waveform_interval); // 12*3 indices starting at 0 -> 12 triangles
+
 
 		glUniform1i(objectID, 0);
 		// Send our transformation to the currently bound shader, 
@@ -372,13 +376,22 @@ int main(int argc, char **argv)
 			sum_l += abs(((short*)data.data)[data_index++]);
 			sum_r += abs(((short*)data.data)[data_index++]);
 		}
-		Model[1][1] = sum_l / bpf / 32768 * cube_height;
-		//Model[1][1] = sum_l / 32768 * cube_height;
-		MVP = PV * Model;
-		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
+		sum_l /= bpf; //avg
+		sum_r /= bpf;
 
+		Model = glm::mat4(
+			 1.0, 0.0, 0.0, 0.0,
+			 0.0, 1.0, 0.0, 0.0,
+			 0.0, 0.0, 1.0, 0.0,
+			-2.0, 1.0, 0.0, 1.0);
+		glm::mat4 scale1 = glm::mat4(
+			1.0, 0.0, 0.0, 0.0,
+			0.0, sum_l / 32768 * cube_height, 0.0, 0.0,
+			0.0, 0.0, 1.0, 0.0,
+			0.0, 0.0, 0.0, 1.0);
+		MVP = PV * scale1 * Model;
+		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
 		// 1rst attribute buffer : vertices
-//		glEnableVertexAttribArray(0);
 		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer1);
 		glVertexAttribPointer(
 			0,                  // attribute. No particular reason for 0, but must match the layout in the shader.
@@ -388,9 +401,7 @@ int main(int argc, char **argv)
 			0,                  // stride
 			(void*)0            // array buffer offset
 		);
-
 		// 2nd attribute buffer : colors
-//		glEnableVertexAttribArray(1);
 		glBindBuffer(GL_ARRAY_BUFFER, colorbuffer1);
 		glVertexAttribPointer(
 			1,                                // attribute. No particular reason for 1, but must match the layout in the shader.
@@ -400,17 +411,22 @@ int main(int argc, char **argv)
 			0,                                // stride
 			(void*)0                          // array buffer offset
 		);
-
 		// Draw the triangle !
 		glDrawArrays(GL_TRIANGLES, 0, 12*3); // 12*3 indices starting at 0 -> 12 triangles
 
-		Model[1][1] = sum_r / bpf / 32768 * cube_height;
-		//Model[1][1] = sum_r / 32768 * cube_height;
-		MVP = PV * Model;
+		Model = glm::mat4(
+			 1.0, 0.0, 0.0, 0.0,
+			 0.0, 1.0, 0.0, 0.0,
+			 0.0, 0.0, 1.0, 0.0,
+			 2.0, 1.0, 0.0, 1.0);
+		glm::mat4 scale2 = glm::mat4(
+			1.0, 0.0, 0.0, 0.0,
+			0.0, sum_r / 32768 * cube_height, 0.0, 0.0,
+			0.0, 0.0, 1.0, 0.0,
+			0.0, 0.0, 0.0, 1.0);
+		MVP = PV * scale2 * Model;
 		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
-
 		// 1rst attribute buffer : vertices
-//		glEnableVertexAttribArray(0);
 		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer2);
 		glVertexAttribPointer(
 			0,                  // attribute. No particular reason for 0, but must match the layout in the shader.
@@ -420,9 +436,7 @@ int main(int argc, char **argv)
 			0,                  // stride
 			(void*)0            // array buffer offset
 		);
-
 		// 2nd attribute buffer : colors
-//		glEnableVertexAttribArray(1);
 		glBindBuffer(GL_ARRAY_BUFFER, colorbuffer2);
 		glVertexAttribPointer(
 			1,                                // attribute. No particular reason for 1, but must match the layout in the shader.
@@ -432,12 +446,12 @@ int main(int argc, char **argv)
 			0,                                // stride
 			(void*)0                          // array buffer offset
 		);
-
 		// Draw the triangle !
 		glDrawArrays(GL_TRIANGLES, 0, 12*3); // 12*3 indices starting at 0 -> 12 triangles
 
 		glDisableVertexAttribArray(0);
 		glDisableVertexAttribArray(1);
+		glDisableVertexAttribArray(2);
 
 		// Swap buffers
 		glfwSwapBuffers(window);
@@ -450,12 +464,12 @@ int main(int argc, char **argv)
 		last_time = current_time;
 		//if(current_time > 0.1) usleep(delta * 1000000);
 		usleep(delta * 1000000);
-		//printf("%d\n", data.size);
 		glfwPollEvents();
 	} // Check if the ESC key was pressed or the window was closed
 	while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
 		   glfwWindowShouldClose(window) == 0 );
 
+	pthread_cancel(a_thread);
 	res = pthread_join(a_thread, &thread_result);
 	if(res != 0) {
 		perror("Thread join failed!");
@@ -477,4 +491,3 @@ int main(int argc, char **argv)
 
 	return 0;
 }
-
